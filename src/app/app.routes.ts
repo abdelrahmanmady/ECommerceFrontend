1523e4e053416b6cab920136b6641a58522dc511
NoranTarekf953 <ntsm2001@gmail.com>
2025-11-22 22:24:59 +0200

    Added product details page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -9,6 +9,9 @@ export const routes: Routes = [
         path: 'login',
         loadComponent: () => import('./features/auth/login/login').then(m => m.Login)
     },
-    
+    {
+        path : 'product-details',
+        loadComponent:() => import('./features/shop/product-details/product-details').then(m=>m.ProductDetails)
+    }
 
 ];
