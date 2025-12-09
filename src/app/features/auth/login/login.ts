52dee786ea8c24f266aba38b50338bded6a57f55
mohamedsamy3110 <memo.samy3110@gmail.com>
2025-12-09 19:38:46 +0200

    Fix pages routing

--- src/app/features/auth/login/login.ts
+++ src/app/features/auth/login/login.ts
@@ -1,10 +1,11 @@
 import { NgClass } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-login',
-  imports: [FormsModule,NgClass],
+  imports: [FormsModule, NgClass, RouterLink],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
--- src/app/features/cart/cart.ts
+++ src/app/features/cart/cart.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-cart',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './cart.html',
   styleUrl: './cart.css',
 })
