546c4535e37826852eeac0bb280ef5f0e0086ff6
mohamedsamy3110 <memo.samy3110@gmail.com>
2025-12-09 19:02:41 +0200

    Add support page

---
+++ src/app/features/support/support.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterLink } from "@angular/router";
+
+@Component({
+  selector: 'app-support',
+  imports: [RouterLink],
+  templateUrl: './support.html',
+  styleUrl: './support.css',
+})
+export class Support {
+
+}
