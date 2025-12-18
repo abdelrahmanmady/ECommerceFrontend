d8f557bdbdf7b0a77616168ffa3a9c57fe1deb68
abdelrahmanmady <adomady55@outlook.com>
2025-12-18 05:43:22 +0200

    went local for development

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: false,
 
-  url: 'https://finalproject1.runasp.net'
+  // url: 'https://finalproject1.runasp.net'
 
-  //for loacal
-  // url: 'https://localhost:5024'
+  // for local
+  url: 'https://localhost:5024'
 };
